8d3830289da7cf389de6103c7eb8e08f41eade68
Jussi Lind <jussi.lind@iki.fi>
2018-07-12 14:28:53 +0300

    Revert "Use QMake for Windows build as CMake has problems with static Qt5"
    This reverts commit 47f853b557de09c52c75ccf67653495ef60ae611.

--- scripts/buildWindowsInstaller.sh
+++ scripts/buildWindowsInstaller.sh
@@ -2,9 +2,10 @@
 
 # Builds NSIS installer for Windows in Docker
 
-QMAKE=/opt/mxe/usr/bin/i686-w64-mingw32.static-qmake-qt5
+CMAKE=/opt/mxe/usr/bin/i686-w64-mingw32.static-cmake
+CPACK=/opt/mxe/usr/bin/i686-w64-mingw32.static-cpack
 
-CMD="export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && export PATH='$PATH':/opt/mxe/usr/bin && cd /heimer && mkdir -p build-windows-docker && cd build-windows-docker && ${QMAKE} .. && make -j3"
+CMD="export LANG=en_US.UTF-8 && export LC_ALL=en_US.UTF-8 && export PATH='$PATH':/opt/mxe/usr/bin && cd /heimer && mkdir -p build-windows-docker && cd build-windows-docker && ${CMAKE} -DCMAKE_BUILD_TYPE=Release .. && ${CMAKE} --build . --target all -- -j3 && ${CPACK} -G NSIS64"
 
 if [ -f /.dockerenv ]; then
     echo "Script inside Docker"
